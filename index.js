2488c18ceb8f31948ae55ae4a453ed155ab3b0b6
Mostafa Imam <137308525+mostafa-imam@users.noreply.github.com>
2026-02-23 21:24:55 +0200

    Add hash map tests

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import { LinkedList } from "./linked-list.js";
 
+import { HashMap } from "./hash-map.js";
+
+
 /* linked list tests */
 // const list = new LinkedList();
 // 
@@ -15,3 +18,47 @@ import { LinkedList } from "./linked-list.js";
 // console.log(`Head of Linked List: ${list.getHead()}`);
 // console.log(`Tail of Linked List: ${list.tail()}`);
 // console.log(`Linked List Items: ${list.toString()}`);
+
+/* hash map Tests */
+// const test = new HashMap();
+// 
+// test.set("apple", "red");
+// test.set("banana", "yellow");
+// test.set("carrot", "orange");
+// test.set("dog", "brown");
+// test.set("elephant", "gray");
+// test.set("frog", "green");
+// test.set("grape", "purple");
+// test.set("hat", "black");
+// test.set("ice cream", "white");
+// test.set("jacket", "blue");
+// test.set("kite", "pink");
+// test.set("lion", "golden");
+// console.log(test.capacity);
+// console.log(test.buckets);
+// 
+// console.log("----------------------------------");
+// 
+// test.set("moon", "silver");
+// test.set("jacket", "black");
+// test.set("ice cream", "orange");
+// 
+// console.log(test.get("dog"));
+// 
+// console.log(test.has("lion"));
+// console.log(test.has("bird"));
+// 
+// console.log(test.remove("kite"));
+// console.log(test.has("kite"));
+// 
+// console.log(test.length());
+// console.log(test.keys());
+// console.log(test.values());
+// console.log(test.entries());
+// 
+// console.log(test.capacity);
+// console.log(test.buckets);
+// 
+// console.log(test.clear());
+// console.log(test.capacity);
+// console.log(test.buckets);
