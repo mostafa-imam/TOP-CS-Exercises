6c1fd84944eef6d60736cbeb64dec4fcd41ddb46
Mostafa Imam <137308525+mostafa-imam@users.noreply.github.com>
2026-02-23 21:20:38 +0200

    Add hash map linked list solution

---
+++ hash-map-linked-list.js
@@ -0,0 +1,15 @@
+export { Node, LinkedList };
+
+class Node {
+    constructor() {
+        this.key = null;
+        this.value = null;
+        this.nextNode = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+}
