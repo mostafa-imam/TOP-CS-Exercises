335aa3971f677befbc9a6324121e9238b7b333f0
Mostafa Imam <137308525+mostafa-imam@users.noreply.github.com>
2026-02-23 21:22:56 +0200

    Add linked list tests

---
+++ index.js
@@ -0,0 +1,17 @@
+import { LinkedList } from "./linked-list.js";
+
+/* linked list tests */
+// const list = new LinkedList();
+// 
+// list.append("dog");
+// list.append("cat");
+// list.append("parrot");
+// list.append("hamster");
+// list.append("snake");
+// list.append("turtle");
+// list.prepend("hippo");
+// 
+// console.log(`Linked List Size: ${list.size()}`);
+// console.log(`Head of Linked List: ${list.getHead()}`);
+// console.log(`Tail of Linked List: ${list.tail()}`);
+// console.log(`Linked List Items: ${list.toString()}`);
