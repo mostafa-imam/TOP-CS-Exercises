cbc932eca4723389e6871095b502dbd62819507c
Mostafa Imam <137308525+mostafa-imam@users.noreply.github.com>
2026-02-24 23:33:09 +0200

    Add insert(value) in Tree class

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -52,19 +52,40 @@ class Tree {
 
     return false;
   }
+
+  insert(value) {
+    this.root = this.#insertRec(this.root, value);
+  }
+
+  #insertRec(root, value) {
+    if (root === null) return new Node(value);
+
+    if (value < root.data) {
+      root.left = this.#insertRec(root.left, value);
+    } else if (value > root.data) {
+      root.right = this.#insertRec(root.right, value);
+    }
+
+    return root;
+  }
 }
 
-// const prettyPrint = (node, prefix = "", isLeft = true) => {
-//     if (node === null || node === undefined) {
-//         return;
-//     }
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null || node === undefined) {
+    return;
+  }
 
-//     prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
-//     console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
-//     prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
-// };
+  prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+};
 
 const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
-// prettyPrint(tree.root);
 // console.log(tree.root);
-console.log(tree.includes(67));
+// console.log(tree.includes(67));
+// tree.insert(100);
+// tree.insert(6);
+// prettyPrint(tree.root);
+// tree.insert(7);
+tree.insert(99);
+prettyPrint(tree.root);
